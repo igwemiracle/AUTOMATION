d1420b0babe398d7207163b53abb8fbc3e666eeb
igwemiracle <igwemiracle35@gmail.com>
2023-08-08 09:47:44 +0100

    automate file

--- automation.py
+++ automation.py
@@ -14,11 +14,3 @@ elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 
 driver.close()
-
-# fire_browser.get('https://www.seleniumeasy.com/tags/allure')
-
-# assert "Allure" in fire_browser.title
-# allure_btn = fire_browser.find_element(By.CLASS_NAME, "active")
-# print(allure_btn.get_attribute('innerHTML'))
-
-# fire_browser.close()
